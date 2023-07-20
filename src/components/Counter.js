444ce74c3914ef581d8fb1ff5d523eb41c92b7f3
vitamaxoduol <vitamaxoduo@gmail.com>
2023-07-20 23:06:32 +0300

    Completed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,19 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // console.log(`before setState: ${count}`);
+
+
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+
+    // console.log(count);
+
+    // setCount(count + 1);
+
+    // console.log(count);
+
+    // console.log(`after setState: ${count}`);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
